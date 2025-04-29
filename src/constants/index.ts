16710d92078461998f782bcbf42a238e41933cab
ubanillx <longxin_liu@qq.com>
2025-04-30 01:03:01 +0800

    build: 更新后端服务地址- 本地后端地址从 "http://localhost:12345/" 修改为 "http://10.16.62.100:12345/" - 线上后端地址从 "https://yupi.icu" 修改为 "http://backend.smartclass.ubanillx.cn"

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,9 +1,9 @@
 /**
  * 本地后端地址
  */
-export const BACKEND_HOST_LOCAL = "http://localhost:12345/";
+export const BACKEND_HOST_LOCAL = "http://10.16.62.100:12345/";
 
 /**
  * 线上后端地址
  */
-export const BACKEND_HOST_PROD = "https://yupi.icu";
+export const BACKEND_HOST_PROD = "http://backend.smartclass.ubanillx.cn";
