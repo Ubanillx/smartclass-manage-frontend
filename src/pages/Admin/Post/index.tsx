52eab99f4af99595dfec353c5bdee66f371b7432
ubanillx <longxin_liu@qq.com>
2025-04-30 01:03:51 +0800

    refactor(admin): 重构课程管理、帖子管理页面并更新欢迎页面
    - 移除了 Chat 页面的空实现
    - 重新设计了 Course 和 Post 页面的布局和样式
    - 更新了 Welcome 页面的内容，增加了系统介绍和快捷入口

--- src/pages/Admin/Post/index.tsx
+++ src/pages/Admin/Post/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Card, Typography } from 'antd';
+import { FormOutlined } from '@ant-design/icons';
+
+const { Title } = Typography;
 
 const PostManagement: React.FC = () => {
   return (
-    <div>
-
+    <div className="post-management">
+      <Card>
+        <div style={{ display: 'flex', alignItems: 'center', marginBottom: 16 }}>
+          <FormOutlined style={{ fontSize: 24, marginRight: 8, color: '#fa8c16' }} />
+          <Title level={4} style={{ margin: 0 }}>帖子管理</Title>
+        </div>
+        <div>
+          {/* 这里将来添加帖子管理的具体内容 */}
+        </div>
+      </Card>
     </div>
-
-  )
+  );
 };
 
 export default PostManagement;
