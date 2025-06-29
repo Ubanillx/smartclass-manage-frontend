d905bad79e8b356431060ed6cb16eb399e1c28fe
Ubanillx <liulongxin21@gmail.com>
2025-06-29 15:15:50 +0800

    feat(backend): 添加 AI阿凡达管理员接口并优化列表接口
    - 新增 updateAiAvatarAdmin 接口，用于更新 AI 阿凡达管理员信息- 新增 listAiAvatarByPageAdmin 接口，用于分页获取 AI 阿凡达列表
    - 优化 listAiAvatar接口，明确其管理员权限

--- src/services/backend/aiAvatarController.ts
+++ src/services/backend/aiAvatarController.ts
@@ -64,13 +64,47 @@ export async function deleteAiAvatarUsingDelete(
   });
 }
 
+/** updateAiAvatarAdmin PUT /api/ai-avatars/${param0}/admin */
+export async function updateAiAvatarAdminUsingPut(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.updateAiAvatarAdminUsingPUTParams,
+  body: API.AiAvatar,
+  options?: { [key: string]: any },
+) {
+  const { id: param0, ...queryParams } = params;
+  return request<API.BaseResponseBoolean_>(`/api/ai-avatars/${param0}/admin`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    params: { ...queryParams },
+    data: body,
+    ...(options || {}),
+  });
+}
+
 /** listAiAvatar GET /api/ai-avatars/admin */
 export async function listAiAvatarUsingGet(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: API.listAiAvatarUsingGETParams,
   options?: { [key: string]: any },
 ) {
-  return request<API.BaseResponseListAiAvatarVO_>('/api/ai-avatars/admin', {
+  return request<API.BaseResponseListAiAvatar_>('/api/ai-avatars/admin', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  });
+}
+
+/** listAiAvatarByPageAdmin GET /api/ai-avatars/admin/page */
+export async function listAiAvatarByPageAdminUsingGet(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.listAiAvatarByPageAdminUsingGETParams,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponsePageAiAvatar_>('/api/ai-avatars/admin/page', {
     method: 'GET',
     params: {
       ...params,
